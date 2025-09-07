876326b19e0540dd912c457bd50377249d1a0e93
Salma <sa1963645@gmail.com>
2025-09-07 16:03:52 +0300

    Initialize server.js file for application setup

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fs from "node:fs";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+// middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+const server = async () => {
+  try {
+    await connect();
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log("Failed to strt server.....", error.message);
+    process.exit(1);
+  }
+};
+
+server();
